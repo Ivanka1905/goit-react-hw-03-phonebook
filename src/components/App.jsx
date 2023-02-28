5caa8ae124a36f46f4b9118d696e551dd863caf0
Ivanka Nagornyuk <IvNagornyuk@gmail.com>
2023-03-01 01:32:36 +0200

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,10 +10,10 @@ import { Container } from 'components/App.styled';
 class App extends Component {
   state = {
     contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
     filter: '',
   };
@@ -53,6 +53,20 @@ class App extends Component {
       contacts: contacts.filter(c => c.id !== deleteId),
     }));
   };
+
+  componentDidMount() {
+    const contacts = localStorage.getItem('contact');
+    const parsedContacts = JSON.parse(contacts);
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contact', JSON.stringify(this.state.contacts));
+    }
+  }
 
   render() {
     const { filterContacts, getDataOnSubmit, changeFilter, deleteContact } =
